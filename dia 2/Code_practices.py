732039ce2cb4b9196d0bbd74622eee3af6912c00
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-02 16:22:35 -0600

    day 2/practice exercises 19-21

--- dia 2/Code_practices.py
+++ dia 2/Code_practices.py
@@ -84,3 +84,42 @@ num1 = "7.5"
 num2 = "10"
 
 print(float(num1) + float(num2))
+
+# Práctica Formatear Cadenas 21
+# Necesitamos imprimir el nombre y número de asociado dentro de la siguiente frase:
+
+# Estimado/a (nombre_asociado), su número de asociado es: (numero_asociado)
+
+# Recuerda que la precisión de tu respuesta (espacios, ortografía y puntuación), es muy importante para llegar al resultado correcto.
+
+nombre_asociado = "Jesse Pinkman"
+numero_asociado = 399058
+
+print(f"Estimado/a {nombre_asociado}, su número de asociado es: {numero_asociado}")
+
+# Práctica Formatear Cadenas 20
+# Muestra al usuario la cantidad de puntos acumulados dentro de la siguiente frase:
+
+# Has ganado (puntos_nuevos) puntos! En total, acumulas (puntos_totales) puntos
+
+# Recuerda que la precisión de tu respuesta (espacios, ortografía y puntuación), es muy importante para llegar al resultado correcto.
+
+puntos_nuevos = 350
+puntos_totales = 1225
+
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos")
+
+
+# Práctica Formatear Cadenas 21
+# Muestra al usuario la cantidad de puntos acumulados dentro de la siguiente frase:
+
+# Has ganado (puntos_nuevos) puntos! En total, acumulas (puntos_totales) puntos
+
+# En esta ocasión, la cantidad de puntos acumulados (totales) será igual a los puntos_anteriores más los puntos_nuevos.
+
+# Recuerda que la precisión de tu respuesta (espacios, ortografía y puntuación), es muy importante para llegar al resultado correcto.
+
+puntos_anteriores = 875
+puntos_nuevos = 350
+
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_anteriores + puntos_nuevos} puntos")
