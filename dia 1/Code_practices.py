7bc5fc6250c211c88de3f2fb8f3ee8749a9ed0f9
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-01 20:30:04 -0600

    day 1/practice 7-9 (input)

--- dia 1/Code_practices.py
+++ dia 1/Code_practices.py
@@ -37,3 +37,23 @@ print("G\tH\tI")
     
 print("Barra Normal: /")
 print("Barra Invertida: \\")
+
+#Practica 7
+#Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+#¿Qué estás estudiando?x
+#El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+print(input("¿Qué estás estudiando?"))
+
+#Practica 8
+#Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+#¿En qué país vives?
+#El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+print(input("¿En qué país vives?"))
+
+#Practica 9 
+#Crea un código que muestre en pantalla el nombre completo del usuario, permitiéndole ingresar su nombre y apellido con las siguientes instrucciones:
+
+#Escribe tu nombre:
+#Escribe tu apellido:
+#El código debe poder imprimir en pantalla el nombre y apellido del usuario, separados por un espacio.
+print(input("Escribe tu nombre:") + " " + input("Escribe tu apellido:"))
