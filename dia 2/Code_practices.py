bbd2c6bd547bee76aba03f338d5b8f492ab776e1
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-02 16:31:06 -0600

    day 2/practice exercises 25-27

--- dia 2/Code_practices.py
+++ dia 2/Code_practices.py
@@ -145,3 +145,19 @@ print(456%33)
 # Debes mostrar solo el valor numérico que resulta de esta operación.
 
 print(783**0.5)
+
+# Práctica Redondeo 25
+# Redondea el resultado de la división 10/3 a un número con 2 decimales, y muestra en pantalla el valor redondeado.
+
+print(round(10/3,2))
+
+# Práctica Redondeo 26
+# Redondea el número 10.676767 al entero más próximo, y muestra en pantalla el resultado.
+
+valor = 10.676767
+print(round(valor))
+
+# Práctica Redondeo 27
+# Calcula la raíz cuadrada de 5, y muestra en pantalla el resultado redondeado con 4 posiciones decimales.
+
+print(round(5**0.5,4))
