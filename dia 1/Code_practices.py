5708383ea1556307cc9dea86a8ae38928b47942c
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-01 18:21:01 -0600

    day 1/practice of strings

--- dia 1/Code_practices.py
+++ dia 1/Code_practices.py
@@ -9,3 +9,31 @@ print('Estudiar con "Python Total" es super divertido')
 #Practica 3
 #Crea un código que imprima en pantalla el número 555, pero no debes imprimirlo directamente, sino como resultado de una operación matemática
 print(500 + 55)
+
+#Practica 4
+#Crea un código que imprima en pantalla la siguiente expresión (pero usando print una sola vez):
+#Línea 1
+#Línea 2
+#Línea 3
+
+print("Línea 1\nLínea 2\nLínea 3")
+
+#Practica 5
+#Crea un código que imprima en pantalla la siguiente expresión.
+#A  B   C
+#D  E   F
+#G  H   I
+#Notas:
+#Observa que la distancia horizontal entre los números ha sido creada con UNA tabulación.
+#Puedes usar una o más declaraciones print
+print("A\tB\tC")
+print("D\tE\tF")
+print("G\tH\tI")
+
+#Practica 6
+#Crea un código que imprima en pantalla la siguiente expresión:
+#Barra Normal: /
+#Barra Invertida: \
+    
+print("Barra Normal: /")
+print("Barra Invertida: \\")
