64ad60b96f767bc30c80ff09ed0da2428169e6a3
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-02 16:15:53 -0600

    day 2/practice exercises 16-18

--- dia 2/Code_practices.py
+++ dia 2/Code_practices.py
@@ -61,3 +61,26 @@ num2 = 2.5
 
 print(type(num1 + num2))
 
+
+# Práctica con Conversiones 16
+# Convierte el valor de num1 en un int e imprime el tipo de dato que resulta:
+
+num1 = 7.5
+print(type(int(num1)))
+
+
+# Práctica con Conversiones 17
+# Convierte el valor de num2 en un float e imprime el tipo de dato que resulta:
+
+num2 = 10
+print(type(float(num2)))
+
+# Práctica con Conversiones 18
+# Suma los valores de num1 y num2.
+
+# No modifiques el valor de las variables ya declaradas, sino aplica las conversiones necesarias dentro de la función print().
+
+num1 = "7.5"
+num2 = "10"
+
+print(float(num1) + float(num2))
