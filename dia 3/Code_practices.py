659c924bae5f69d7de7b3e023dd37a753a17093a
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-03 12:04:19 -0600

    day 3/ practice exercises 28-33

---
+++ dia 3/Code_practices.py
@@ -0,0 +1,30 @@
+# Práctica Método Index() 1
+
+palabra = "ordenador"
+ 
+print(palabra[4])
+# Práctica Método Index() 2
+
+frase = "En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+ 
+print(frase.index("práctica"))
+# Práctica Método Index() 3
+
+frase = "En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+ 
+print(frase.rindex("práctica"))
+# Práctica Sub-Strings 1
+
+frase = "Controlar la complejidad es la esencia de la programación"
+ 
+print(frase[:9])
+# Práctica Sub-Strings 2
+
+frase = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+ 
+print(frase[8::3])
+# Práctica Sub-Strings 3
+
+frase = "Es genial trabajar con ordenadores. No discuten, lo recuerdan todo y no se beben tu cerveza"
+ 
+print(frase[::-1])
