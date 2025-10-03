7727d6dc891111ecac1db4384e408235c6e2f0cb
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-03 13:58:32 -0600

    day 3/practice exercises 40-42

--- dia 3/Code_practices.py
+++ dia 3/Code_practices.py
@@ -69,3 +69,16 @@ print("agua" not in haiku)
 palabra = "electroencefalografista"
  
 print(len(palabra))
+
+# Práctica Listas 1
+
+mi_lista = ["uno", 2, 3.33, "four", True]
+# Práctica Listas 2
+
+medios_transporte = ["avión", "auto", "barco", "bicicleta"]
+medios_transporte.append("motocicleta")
+# Práctica Listas 3
+
+frutas = ["manzana", "banana", "mango", "cereza", "sandía"]
+ 
+eliminado = frutas.pop(2)
