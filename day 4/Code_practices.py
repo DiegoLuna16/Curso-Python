b53ea5e3850460e59bed23cbe3507f8a02495f4a
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-05 16:28:09 -0600

    day 4/ PE 85-87

--- day 4/Code_practices.py
+++ day 4/Code_practices.py
@@ -224,3 +224,19 @@ from random import *
 nombres = ["Carlos", "Julia", "Nicole", "Laura", "Mailen"]
  
 sorteo = choice(nombres)
+
+# Práctica Comprensión de Listas 1
+
+valores = [1, 2, 3, 4, 5, 6, 9.5]
+ 
+valores_cuadrado = [valor**2 for valor in valores]
+# Práctica Comprensión de Listas 2
+
+valores = [1, 2, 3, 4, 5, 6, 9.5]
+ 
+valores_pares = [valor for valor in valores if valor%2 == 0]
+# Práctica Comprensión de Listas 3
+
+temperatura_fahrenheit = [32, 212, 275]
+ 
+grados_celsius = [(temperatura-32)*(5/9) for temperatura in temperatura_fahrenheit]
