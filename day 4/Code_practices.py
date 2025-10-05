a4d27525855a08abd61552d08bbc87728b582ae0
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-05 14:23:41 -0600

    day 4/ PE 67-69

--- day 4/Code_practices.py
+++ day 4/Code_practices.py
@@ -111,3 +111,30 @@ for numero in lista_numeros:
         suma_pares = suma_pares + numero
     else:
         suma_impares = suma_impares + numero
+        
+# Práctica Loop While 1
+
+numero = 10
+ 
+while numero >= 0:
+    print(numero)
+    numero -= 1
+# Práctica Loop While 2
+
+numero = 50
+ 
+while numero >= 0:
+    if numero % 5 == 0:
+        print(numero)
+        numero -= 1
+    else:
+        numero -= 1
+# Práctica Interrupción de Flujo
+
+lista_numeros = [4,5,8,7,6,9,8,2,4,5,7,1,9,5,6,-1,-5,6,-6,-4,-3]
+ 
+for numero in lista_numeros:
+    if numero >= 0:
+        print(numero)
+    else:
+        break
