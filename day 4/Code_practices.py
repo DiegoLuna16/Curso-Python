f3cf38f6bdf5c5124d224012e582e9ed0b5bed8e
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-05 14:08:27 -0600

    day 4/ PE 64-66

--- day 4/Code_practices.py
+++ day 4/Code_practices.py
@@ -81,5 +81,33 @@ else:
     else:
         if not habla_ingles:
             print("Para postularte, necesitas tener conocimientos de inglés")
-        if not sabe_python:
+        else:
             print("Para postularte, necesitas saber programar en Python")
+
+# Práctica Loop For 1
+
+alumnos_clase = ["María", "José", "Carlos", "Martina", "Isabel", "Tomás", "Daniela"]
+ 
+for alumno in alumnos_clase:
+    print(f"Hola {alumno}")
+# Práctica Loop For 2
+
+lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+ 
+suma_numeros = 0
+ 
+for numero in lista_numeros:
+    suma_numeros = suma_numeros + numero
+# Práctica Loop For 3
+
+lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+ 
+suma_pares = 0
+ 
+suma_impares = 0
+ 
+for numero in lista_numeros:
+    if numero % 2 == 0:
+        suma_pares = suma_pares + numero
+    else:
+        suma_impares = suma_impares + numero
