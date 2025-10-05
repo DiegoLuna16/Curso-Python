95d26795625f8ee97c6f3b8bb29e77ca1a80622b
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-05 16:09:49 -0600

    day 4/ PE 79-81

--- day 4/Code_practices.py
+++ day 4/Code_practices.py
@@ -189,3 +189,20 @@ portugues = ["um", "dois", "três", "quatro", "cinco"]
 ingles = ["one", "two", "three", "four", "five"]
  
 numeros = list(zip(espaniol, portugues, ingles))
+
+# Práctica Min y Max 1
+
+lista_numeros = [44542247/2, 21310/5, 2134747*33, 44556475, 121676, 6654067, 353254, 123134, 55**12, 611**5]
+ 
+valor_maximo = max(lista_numeros)
+# Práctica Min y Max 2
+
+lista_numeros = [44542247, 21310, 2134747, 44556475, 121676, 6654067, 353254, 123134, 552512, 611665]
+ 
+rango = max(lista_numeros) - min(lista_numeros)
+# Práctica Min y Max 3
+
+diccionario_edades = {"Carlos":55, "María":42, "Mabel":78, "José":44, "Lucas":24, "Rocío":35, "Sebastián":19, "Catalina":2,"Darío":49}
+ 
+edad_minima = min(diccionario_edades.values())
+ultimo_nombre = max(diccionario_edades.keys())
