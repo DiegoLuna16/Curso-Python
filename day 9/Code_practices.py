3e34c9625f17bdce2f2f156530e607658b0328ca
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-11 14:35:33 -0600

    day 9/ PE 160-162

--- day 9/Code_practices.py
+++ day 9/Code_practices.py
@@ -19,17 +19,14 @@ lista_ciudades= deque(["Londres", "Berlin", "París", "Madrid", "Roma", "Moscú"
 
 # Práctica Módulo Datetime 1
 
-from datetime import date
+from datetime import date, datetime
  
 mi_fecha = date(1999, 2, 3)
 # Práctica Módulo Datetime 2
 
-from datetime import date
- 
+
 hoy = date.today()
 # Práctica Módulo Datetime 3
-
-from datetime import datetime
  
 minutos = datetime.now().minute
 
@@ -39,12 +36,41 @@ import math
  
 resultado = math.log10(25)
 # Práctica Módulo Math 2
-
-import math
  
 resultado = math.sqrt(math.pi)
 # Práctica Módulo Math 3
-
-import math
  
 resultado = math.factorial(7)
+
+# Práctica Módulo RE 1
+
+import re
+ 
+ 
+def verificar_email(email):
+    patron = r'@\w+\.com'
+    verificar = re.search(patron,email)
+    if verificar:
+        print("Ok")
+    else:
+        print("La dirección de email es incorrecta")
+# Práctica Módulo RE 2
+
+ 
+def verificar_saludo(frase):
+    patron = r'^Hola'
+    verificar = re.search(patron,frase)
+    if verificar:
+        print("Ok")
+    else:
+        print("No has saludado")
+# Práctica Módulo RE 3
+
+
+def verificar_cp(cp):
+    patron = r'\w{2}\d{4}'
+    verificar = re.search(patron,cp)
+    if verificar:
+        print("Ok")
+    else:
+        print("El código postal ingresado no es correcto")
