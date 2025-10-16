02d7bef8705fc143b3ad524f59d528446f440a9e
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-16 16:49:25 -0600

    day 10/ changing title, icon and background color of the display

--- day 10/game.py
+++ day 10/game.py
@@ -1,13 +1,21 @@
 import pygame
-
+#Inicializa pygame
 pygame.init()
 
+#Crear pantalla
 display = pygame.display.set_mode((800,600))
 
-is_running = True
+#Titulo e Icono 
+pygame.display.set_caption("Invasion espacial")
+icon = pygame.image.load("space.png")
+pygame.display.set_icon(icon)
 
+#Loop del juego
+is_running = True
 while is_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
-            
+    
+    display.fill((51,23,59))
+    pygame.display.update()
