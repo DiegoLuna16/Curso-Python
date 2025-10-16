92da41e4449676007523fd94f5177262bdfa90de
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-16 17:01:21 -0600

    day 10/ adding player to the display

--- day 10/game.py
+++ day 10/game.py
@@ -10,12 +10,27 @@ pygame.display.set_caption("Invasion espacial")
 icon = pygame.image.load("space.png")
 pygame.display.set_icon(icon)
 
+# Jugador 
+
+img_jugador = pygame.image.load('cohete.png')
+jugador_x = 368
+jugador_y = 536
+
+def jugador():
+    display.blit(img_jugador,(jugador_x,jugador_y))
+
 #Loop del juego
 is_running = True
 while is_running:
+    #RGB
+    display.fill((51,23,59))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
     
-    display.fill((51,23,59))
+    jugador()
+    
     pygame.display.update()
+    
+    
