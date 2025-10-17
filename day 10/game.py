08184b199af5382438e3534e47460f2b3ba129cf
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-16 19:01:23 -0600

    day 10/ adding mobility to the player and setting limits in the display

--- day 10/game.py
+++ day 10/game.py
@@ -10,14 +10,17 @@ pygame.display.set_caption("Invasion espacial")
 icon = pygame.image.load("space.png")
 pygame.display.set_icon(icon)
 
-# Jugador 
+# Variables Jugador 
 
 img_jugador = pygame.image.load('cohete.png')
 jugador_x = 368
 jugador_y = 536
+jugador_x_cambio = 0
 
-def jugador():
-    display.blit(img_jugador,(jugador_x,jugador_y))
+
+#jugador 
+def jugador(x,y):
+    display.blit(img_jugador,(x,y))
 
 #Loop del juego
 is_running = True
@@ -25,12 +28,41 @@ while is_running:
     #RGB
     display.fill((51,23,59))
     
+    #iterar eventos
     for event in pygame.event.get():
+        
+        #Evento cerrar juego
         if event.type == pygame.QUIT:
             is_running = False
+        
+        #Evento presionar flechas
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                jugador_x_cambio -= 0.3
+            elif event.key == pygame.K_RIGHT:
+                jugador_x_cambio += 0.3
+            elif event.key == pygame.K_UP:
+                jugador_y -= 5
+            elif event.key == pygame.K_DOWN:
+                jugador_y += 5
+                
+        #Evento soltar flechas
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                jugador_x_cambio = 0
+                
+    # modificar ubicacion del jugador
+    jugador_x += jugador_x_cambio
     
-    jugador()
+    # mantener dentro de bordes
+    if jugador_x <= 0:
+        jugador_x = 0
+    elif jugador_x >= 736:
+        jugador_x = 736
     
+    jugador(jugador_x,jugador_y)
+    
+    #actualizar
     pygame.display.update()
     
     
