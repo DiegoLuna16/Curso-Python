e489a66c6afb521fcc4a22c5c046f6567df9fd89
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-19 16:29:21 -0600

    day 11/ getting img from website and saving in our local storage

--- day 11/web_scraping.py
+++ day 11/web_scraping.py
@@ -11,5 +11,15 @@ print(parrafo_especial)
 
 columna_lateral = soup.select('.widget-content h3')
 
-for h3 in columna_lateral:
-    print(h3.getText())
+# for h3 in columna_lateral:
+#     print(h3.getText())
+
+imagen = soup.select('.post-body img')[0]['src']
+
+imagen_curso = requests.get(imagen)
+
+f = open('mi_imagen.jpp', 'wb')
+f.write(imagen_curso.content)
+f.close()
+
+    
