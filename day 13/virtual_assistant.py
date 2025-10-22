4bf5c9e0830620863d014aa6f990a8de7b60b3cb
DiegoLuna16 <diegovelazquezp13@gmail.com>
2025-10-22 17:43:39 -0600

    day 13/ Adding multiple options to the virtual assistant

--- day 13/virtual_assistant.py
+++ day 13/virtual_assistant.py
@@ -1,7 +1,7 @@
 import pyttsx3
 import speech_recognition as sr
 import pywhatkit
-import yfinance
+import yfinance as yf
 import pyjokes
 import webbrowser
 import datetime
@@ -120,4 +120,79 @@ def saludo_inicial():
     #decir el saludo
     hablar(f"{momento}, soy Siri, tu asistente personal. Por favor, dime en que te puedo ayudar.")
 
-saludo_inicial()
+#funcion central del asistente
+def pedir_cosas():
+    
+    #activar saludo incial
+    saludo_inicial()
+    
+    #variable de corte
+    
+    comenzar = True
+    
+    #loop central
+    
+    while comenzar:
+        
+        #activar el micro y guardar el pedido en un string 
+        pedido = transformar_audio_en_texto().lower()
+        print(pedido)
+        
+        if  'abrir youtube' in pedido:
+            hablar('con gusto, estoy abriendo youtube')
+            webbrowser.open('https://www.youtube.com')
+            continue
+        elif 'abrir el navegador' in pedido:
+            hablar('claro estoy en eso')
+            webbrowser.open('https://www.google.com')
+            continue
+        elif 'qué día es hoy' in pedido:
+            pedir_dia()
+            continue
+        elif 'qué hora es' in pedido:
+            pedir_hora()
+            continue
+        elif 'busca en wikipedia' in pedido:
+            hablar('Buscando en wikipedia')
+            pedido = pedido.replace('busca en wikipedia','')
+            wikipedia.set_lang('es')
+            resultado = wikipedia.summary(pedido,sentences = 1)
+            hablar('Wikipedia dice lo siguiente:')
+            hablar(resultado)
+            continue
+        elif 'repite' in pedido:
+            pedido = pedido.replace('repite','')
+            hablar(pedido)
+            continue
+        elif 'busca en internet' in pedido:
+            hablar('Ya mismo lo busco')
+            pedido = pedido.replace('busca en internet','')
+            pywhatkit.search(pedido)
+            hablar('Esto es lo que encontré')
+            continue
+        elif 'reproduce' in pedido:
+            hablar('Buena idea, ya comienzo a reproducirlo')
+            pedido = pedido.replace('reproduce','')
+            pywhatkit.playonyt(pedido)
+            continue
+        elif 'chiste' in pedido:
+            hablar(pyjokes.get_joke('es'))
+        elif 'precio de las acciones' in pedido:
+            accion = pedido.split('de')[-1].strip()
+            cartera = {'apple': 'APPL',
+                       'amazon':'AMZN',
+                       'google':'GOOGL'}
+            try:
+                accion_buscada = cartera[accion]
+                accion_buscada = yf.Ticker(accion_buscada)
+                precio_actual = accion_buscada.info['regularMarketPrice']
+                hablar(f'La encontré, el precio de {accion} es {precio_actual}')
+                continue
+            except:
+                hablar('Perdón pero no lo he encontrado')
+                continue
+        elif 'salir' in pedido:
+            hablar('Hasta luego')
+            comenzar = False
+            
+pedir_cosas()
